012431775957454995a7f41503e7bd78a5930627
Brian Takita <brian.takita@gmail.com>
2023-10-11 16:04:01 -0400

    + Post__schema
    ∋ zod

--- src/types/Post.ts
+++ src/types/Post.ts
@@ -1,3 +1,15 @@
+import { z } from 'zod'
+import type { ZodType } from 'zod/lib/types'
+export const Post__schema = z.object({
+	title: z.string(),
+	description: z.string(),
+	// Transform string to Date object
+	pub_date: z.coerce.date(),
+	updated_date: z.coerce.date().optional(),
+	hero_image: z.string().optional(),
+} as {
+	[k in keyof Post]: ZodType<Post[k]>
+})
 export interface Post {
 	title:string
 	description:string
