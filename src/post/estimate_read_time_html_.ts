febe1bf8bfdfb16b7e2efe2cc6a7f323cbdf9b97
Brian Takita <brian.takita@gmail.com>
2023-10-29 17:05:25 -0400

    estimate_read_time_html_: + nullish case
    dependencies: ∋ @ctx-core/function

--- src/post/estimate_read_time_html_.ts
+++ src/post/estimate_read_time_html_.ts
@@ -1,4 +1,7 @@
-export function estimate_read_time_html_(text:string) {
-	const minutes = Math.ceil(text.split(' ').length / 200)
-	return `READING TIME<span class="mx-2 text-gray-300">•</span>${minutes} minute${minutes > 1 ? 's' : ''}`
+import { type nullish, nullish__none_ } from '@ctx-core/function'
+export function estimate_read_time_html_(_text:string|nullish) {
+	return nullish__none_([_text], text=>{
+		const minutes = Math.ceil(text!.split(' ').length / 200)
+		return `READING TIME<span class="mx-2 text-gray-300">•</span>${minutes} minute${minutes > 1 ? 's' : ''}`
+	})
 }
