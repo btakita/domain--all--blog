ecad66854407ab8acc37112118dbb52f2acc22f6
Brian Takita <brian.takita@gmail.com>
2023-11-06 12:46:47 -0500

    changes:
    	- _be_state_arg_triple_
    	- _be_state_arg_triple_T
    	- be_derive_pair_
    	- be_derive_pair_T
    	- be_state_triple_
    	- be_state_triple_T
    	- props_clean_van__new
    	- Props_w_undefined
    	- TagFunc_w_undefined
    	- Tags_w_undefined
    	- TagNS_w_undefined
    	- Van_w_undefined
    	- VanShape_w_undefined
    	- val__be_derive_pair_
    	- val__be_derive_pair_T
    	- val__be_state_triple_
    	- val__be_state_triple_T
    	- van_
    	- van__set
    	- van_internals_
    	- van_internals_T
    	- van__elementProto_T
    
    dependencies:
    
    	∋ @ctx-core/vanjs
    	update dependencies

--- src/van/H.ts
+++ src/van/H.ts
@@ -1,9 +1,9 @@
 import { be_, type Ctx } from '@ctx-core/object'
+import { type Tags_w_undefined, van_, type VanShape_w_undefined } from '@ctx-core/vanjs'
 import type { VanShape } from 'van-type-delegate'
-import { type Tags_w_undefined } from './props_clean_van__new'
-import { van_ } from './van.ts'
-const _H_ = be_('H_', ctx=>
-	van_(ctx).tags)
-export function H_<V extends VanShape>(ctx:Ctx) {
-	return _H_(ctx) as any as Tags_w_undefined<V>
+const _H_ = be_('H_',
+	ctx=>
+		van_<VanShape_w_undefined<VanShape>>(ctx).tags)
+export function H_<van_T extends VanShape>(ctx:Ctx) {
+  return _H_(ctx) as Tags_w_undefined<van_T>
 }
--- src/van/S.ts
+++ src/van/S.ts
@@ -1,9 +1,8 @@
 import { be_, type Ctx } from '@ctx-core/object'
+import { type Tags_w_undefined, van_, type VanShape_w_undefined } from '@ctx-core/vanjs'
 import type { VanShape } from 'van-type-delegate'
-import { type Tags_w_undefined } from './props_clean_van__new'
-import { van_ } from './van'
 const _S_ = be_('S_', ctx=>
-	van_(ctx).tagsNS('http://www.w3.org/2000/svg'))
-export function S_<V extends VanShape>(ctx:Ctx) {
-  return _S_(ctx) as any as Tags_w_undefined<V>
+	van_<VanShape_w_undefined<VanShape>>(ctx).tagsNS('http://www.w3.org/2000/svg'))
+export function S_<van_T extends VanShape>(ctx:Ctx) {
+	return _S_(ctx) as Tags_w_undefined<van_T>
 }
