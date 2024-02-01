819a89e6e38261cb39465c30aec965b1a168f8e8
Brian Takita <brian.takita@gmail.com>
2024-02-01 05:16:28 -0500

    post__slug__new: fix: ∋ dehydrated_post_meta.title

--- slug/post__slug__new.ts
+++ slug/post__slug__new.ts
@@ -4,6 +4,6 @@ export function post__slug__new(dehydrated_post_meta:dehydrated_post_meta_T) {
 	return (
 		dehydrated_post_meta.slug
 			? slug(dehydrated_post_meta.slug)
-			: slug(dehydrated_post_meta.data.title)
+			: slug(dehydrated_post_meta.title)
 	)
 }
