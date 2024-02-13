22bc2d1497531d463cfc6767c61d539df59c9f3a
Brian Takita <brian.takita@gmail.com>
2024-02-13 10:58:49 -0500

    ./post: - estimate_read_time_html__new

--- post/estimate_read_time_html__new.ts
+++
@@ -1,7 +0,0 @@
-import { type nullish, nullish__none_ } from 'ctx-core/function'
-export function estimate_read_time_html__new(_text:string|nullish) {
-	return nullish__none_([_text], text=>{
-		const minutes = Math.ceil(text!.split(' ').length / 200)
-		return `READING TIME<span class="mx-2 text-gray-300">•</span>${minutes} minute${minutes > 1 ? 's' : ''}`
-	})
-}
