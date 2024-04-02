28d03e835a4b51a6bfa85cf47af5318f5a4f2a01
Brian Takita <brian.takita@gmail.com>
2024-04-01 20:47:34 -0400

    update dependencies

--- post/post_meta.ts
+++ post/post_meta.ts
@@ -59,11 +59,12 @@ export function post_meta__validate(
 export function post_meta__hydrate({
 	pub_date: _pub_date,
 	updated_date: _updated_date,
-	tag_a1: _tag_a1,
+	tag_a1,
 	..._meta
 }:dehydrated_post_meta_T) {
 	return {
 		..._meta,
+		tag_a1,
 		pub_date: new Date(_pub_date),
 		...(_updated_date ? { updated_date: new Date(_updated_date) } : {}),
 	} as post_meta_T
